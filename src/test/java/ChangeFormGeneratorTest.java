57e0ef21f05305bdadf20b3decf2546ba071e641
markhuang1994@gmail.com <mark820328>
2020-03-19 22:35:53 +0800

    [Add]Source Code Review Guide – Diff List table

--- src/test/java/ChangeFormGeneratorTest.java
+++ src/test/java/ChangeFormGeneratorTest.java
@@ -38,20 +38,20 @@ public class ChangeFormGeneratorTest {
 
     private ChangeFormTable tableData() {
         ArrayList<ChangeFormTableRow> tableRows = new ArrayList<>();
-        String s = Arrays.stream(new String[20]).map(x -> "q").collect(Collectors.joining(""));
-        String s1 = Arrays.stream(new String[20]).map(x -> "w").collect(Collectors.joining(""));
-        String s2 = Arrays.stream(new String[20]).map(x -> "e").collect(Collectors.joining(""));
-        String s3 = Arrays.stream(new String[20]).map(x -> "t").collect(Collectors.joining(""));
+        String s = Arrays.stream(new String[10]).map(x -> "q").collect(Collectors.joining(""));
+        String s1 = Arrays.stream(new String[10]).map(x -> "w").collect(Collectors.joining(""));
+        String s2 = Arrays.stream(new String[10]).map(x -> "e").collect(Collectors.joining(""));
+        String s3 = Arrays.stream(new String[10]).map(x -> "r").collect(Collectors.joining(""));
         tableRows.add(new ChangeFormTableRow(s, s1, s2, s3, "asd", "qwe", "床前明月光\r\n疑似地上霜\r\n舉頭望明月\r\n低頭思故鄉"));
-        for (int i = 0; i < 160; i++) {
+        for (int i = 0; i < 16; i++) {
             tableRows.add(new ChangeFormTableRow(
                     String.valueOf(i),
-                    String.valueOf(i + 160),
-                    String.valueOf(i + 320),
-                    String.valueOf(i + 480),
-                    String.valueOf(i + 640),
-                    String.valueOf(i + 800),
-                    String.valueOf(i + 960)
+                    String.valueOf(i + 16),
+                    String.valueOf(i + 32),
+                    String.valueOf(i + 48),
+                    String.valueOf(i + 64),
+                    String.valueOf(i + 80),
+                    String.valueOf(i + 96)
             ));
         }
 
